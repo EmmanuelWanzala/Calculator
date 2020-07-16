a8262d97f7b4c47ce72a160960130cc2ea72dee8
EmmanuelWanzala <wanzalaemmanuel28@gmail.com>
2020-07-16 08:58:12 +0300

    add initial commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var add =function (number1,number2){
+    return number1+number2
+    };
+    var number1=parseInt(prompt("Enter a number:"));
+    var number2=parseInt(prompt("Enter another number:"))
+    alert(add(number1,number2))
+    
+    subtract = function(number3,number4){
+    return number3-number4
+    }
+    
+    var number3=parseInt(prompt("Enter a number"));
+    var number4=parseInt(prompt("Enter another number"));
+    alert(subtract(number3,number4));
+    
